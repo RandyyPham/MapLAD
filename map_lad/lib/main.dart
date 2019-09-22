e03888a7259b31d40bdedb1a23af002302c1db49
Jack Haehl <jackeliash@gmail.com>
2019-09-22 01:36:44 -0700

    Made working scene transition
    Randy, just do the tutorial, the whole thing.

--- map_lad/lib/main.dart
+++ map_lad/lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'src/locations.dart' as locations;
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/gestures.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main(){
+  runApp(MaterialApp(
+    title: 'MapLAD',
+    home: MapViewer(),
+  ));
 }
 
-class _MyAppState extends State<MyApp> {
+class MapViewer extends StatefulWidget {
+  @override
+  _MapViewerState createState() => _MapViewerState();
+}
+class _MapViewerState extends State<MapViewer> {
   final Map<String, Marker> _markers = {};
 
   Future<void> _onMapCreated(GoogleMapController controller) async {
@@ -37,18 +43,28 @@ class _MyAppState extends State<MyApp> {
             appBar: AppBar(
               title: const Text('MapLAD'),
               backgroundColor: Colors.pink[700],
-            ),
-            body: Column(
-              children: <Widget>[
-                GoogleMap(
-                  onMapCreated: _onMapCreated,
-                  initialCameraPosition: CameraPosition(
-                    target: const LatLng(33.4255, -111.94),
-                    zoom: 14,
-                  ),
-                  markers: _markers.values.toSet(),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.menu),
+                  onPressed: () {
+                    print("EA SPorts, it's in the game.");
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => UserInputArea()),
+                    );
+                    //GO TO SCENE 2
+                  }
                 ),
               ],
+            ),
+            body: GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: const LatLng(33.4255, -111.94),
+                zoom: 14,
+              ),
+              markers: _markers.values.toSet(),
+
             )
           /*children: <Widget>[
               Container(
@@ -73,3 +89,41 @@ class _MyAppState extends State<MyApp> {
         ),
       );
 }
+ class UserInputArea extends StatelessWidget {
+  @override
+   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MatLAB'),
+        backgroundColor: Colors.pink[700],
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                print("Beam me up, Scotty!");
+                Navigator.pop(context);
+
+                //GO BACK TO SCENE 1
+              }
+          ),
+        ],
+      ),
+        body: Column(
+    children: <Widget>[
+    Text('Deliver features faster'),
+    Text('Craft beautiful UIs'),
+    TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Enter your username'
+      ),
+    ),
+    Expanded(
+    child: FittedBox(
+    fit: BoxFit.contain, // otherwise the logo will be tiny
+    child: const FlutterLogo(),
+    ),
+    ),
+    ],
+    )
+    );
+ }}
